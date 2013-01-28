6fdf86447825c9aa7bdeb0c3d270f73d02544857
Takuma YOSHITANI <takuma.7@gmail.com>
2013-01-28 19:07:43 +0900

    marker data made as seed

---
+++ markers2db.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'mongo'
+
+include Mongo
+
+@client = MongoClient.new('localhost', 27017)
+@db     = @client['suzuri']
+@coll   = @db['markers']
+
+@coll.remove
+
+marker_id = 0
+File::open('AR_markers.txt'){|f|
+  while line = f.gets
+    @coll.insert({'marker_id' => marker_id, 'marker' => line.chomp})
+    marker_id += 1
+  end
+}
+
+puts "#{@coll.count} records added."
